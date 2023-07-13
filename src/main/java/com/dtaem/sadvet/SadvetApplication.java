f6c9d6aeb938efc4e3ac503ad770512092a8a3b3
Moises Navarro <2015313073@uteq.edu.mx>
2023-07-12 23:01:41 -0600

    Initial commit

---
+++ src/main/java/com/dtaem/sadvet/SadvetApplication.java
@@ -0,0 +1,13 @@
+package com.dtaem.sadvet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SadvetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SadvetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dtaem/sadvet/SadvetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dtaem.sadvet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SadvetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
